5d80ba2984175ae8dfb0b483e0c6d6b9da61aa24
Paulo Roberto Mesquita <pauloroberto@ajtec.eu>
2025-03-31 12:13:22 -0300

    Init Project

---
+++ app/subscriptions/page.tsx
@@ -0,0 +1,5 @@
+const Subscriptions = () => {
+    return <h1>Subscription page</h1>;
+}
+ 
+export default Subscriptions;
---
+++ app/transactions/[id]/page.tsx
@@ -0,0 +1,5 @@
+const Transaction = ({params: { id }}) => {
+    return <h1>Transactio: {id}</h1>;
+}
+ 
+export default Transaction;
